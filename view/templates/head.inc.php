aac4f34c8db9abfc82e1cd381bf645608c9ff26a
ziagrosvenor <ziagrosvenor@live.co.uk>
2014-09-19 15:50:46 +0100

    Add title tag to head template

--- view/templates/head.inc.php
+++ view/templates/head.inc.php
@@ -1,5 +1,6 @@
 <html lang="en">
 	<head>
+		<title>Z-Blog | A light-weight personal blog built in PHP</title>
 		<link href="./view/css/bootstrap.css" rel="stylesheet">
 		<link rel="stylesheet" href="./view/css/blog-theme.css">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
